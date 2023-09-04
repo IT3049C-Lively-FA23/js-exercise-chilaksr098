d1373629dc2a9100c7aa09bc18c82e806283c740
Sasidhar Chilaka <chilaksr@mail.uc.edu>
2023-09-03 21:12:06 -0400

    commit 3.0

--- src/7.equality.js
+++ src/7.equality.js
@@ -2,7 +2,12 @@
 // Example: strictEquality(1, true) => false
 // Example: strictEquality(1, 1) => true
 function strictEquality(val1, val2) {
-  return ;
+  if(val1 === val2){
+    return true;
+  }
+  else{
+    return false;
+  }
 }
 //equality: compare value but not necessarily the type
 // Example: equality(1, true) => true
